cfa7b978613c627e5b2ec7b98772904390b2108f
Ben Cooling <office@bcooling.com.au>
2016-10-04 15:07:34 +1000

    modified configuration for web, added gitignore

--- web.js
+++ web.js
@@ -1,16 +1,18 @@
 module.exports = {
-  parser: 'babel-eslint',
-  extends: 'airbnb/base',
-  plugins: [
-    'react',
-  ],
+  extends: 'airbnb',
   env: {
+    node: true,
     browser: true,
   },
   rules: {
-    'no-console': 0,
-    'no-param-reassign': 0,
-    'max-len': 0,
-    'padded-blocks': 0,
+    "no-console": 0,
+    "no-param-reassign": 0,
+    "max-len": 0,
+    "padded-blocks": 0,
+    "global-require": 0,
+    "no-confusing-arrow": 0,
+    "no-underscore-dangle": 0,
+    "react/jsx-filename-extension": 0,
+    "react/prop-types": 0,
   },
 };
